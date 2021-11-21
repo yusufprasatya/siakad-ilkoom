8c7e09cf4a47b586fee9ae858f712cf2b4277c3f
yusuf040700 <yusuf.marsudien10@gmail.com>
2021-11-22 06:15:43 +0700

    Modified file DosenFactory

--- database/factories/DosenFactory.php
+++ database/factories/DosenFactory.php
@@ -2,7 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Dosen;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use phpDocumentor\Reflection\Types\This;
 
 class DosenFactory extends Factory
 {
@@ -11,10 +13,21 @@ class DosenFactory extends Factory
      *
      * @return array
      */
+    protected $model = Dosen::class;
     public function definition()
     {
+        //Generate Data Dosen
+        $daftar_titel = ["M.Kom", "M.Sc", "M.T", "M.Si"];
         return [
-            //
+            'nid'   => $this->faker->unique()->numerify('99######'),
+            'nama'  => $this->faker->firstName . " " .
+                $this->faker->lastName . " " .
+                $this->faker->randomElement($daftar_titel),
+            'jurusan_id' => $this->faker->numberBetween(
+                1,
+                \App\Models\Jurusan::count()
+            ),
+
         ];
     }
 }
