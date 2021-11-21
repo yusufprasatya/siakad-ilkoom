8af497fd2a6e9e3a49f7d1da7fb54e2375777df5
yusuf040700 <yusuf.marsudien10@gmail.com>
2021-11-22 06:02:52 +0700

    Modified JurusanFactory

--- database/factories/JurusanFactory.php
+++ database/factories/JurusanFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Jurusan;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class JurusanFactory extends Factory
@@ -11,10 +12,16 @@ class JurusanFactory extends Factory
      *
      * @return array
      */
+    protected $model = Jurusan::class;
+
     public function definition()
     {
+        $daftar_jurusan = ["Ilmu Komputer", "Sistem Informasi", "Teknik Informatika"];
         return [
-            //
+            'nama' => $this->faker->unique()->randomElement($daftar_jurusan),
+            'kepala_jurusan' => "Dr. " . $this->faker->firstName . " " .
+                $this->faker->lastName,
+            'daya_tampung'   => $this->faker->numberBetween(5, 8) * 10,
         ];
     }
 }
